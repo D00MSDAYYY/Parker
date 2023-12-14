dc7d64549598131c953f3c591375e79a0fb20c92
hashiramasenju <>
2023-12-15 01:15:47 +0300

    interface is relatively ready

--- src/main.cpp
+++ src/main.cpp
@@ -9,120 +9,145 @@
 using namespace std;
 using namespace TgBot;
 
-enum class FSM_STATE // FSM states
-{
-    START,              // starting
-    AUTORIZATION,       // autorizing
-    AUTORIZATION_CMPLT, // autorizing complete
-    INFO_HELP,          // information and help
-    RQST_PRKNG,         // requesting for parking
-    SUBMIT_RQST,        // submiting request for parking
-    CNCL_RQST,          // cancelling request for parking
-    TIME_SLCTR          // selecting time for parking
+enum class FSM
+{               // FSM states
+    START = 0,  // starting
+    AUTORIZED = 1 ,  // autorized
+    INFO_HELP = 2,  // information and help
+    RQST_PRKNG = 3, // requesting for parking
+    CNCL_RQST = 4,  // cancelling request for parking
+    SUBMIT_RQST = 5 // submiting request for parking
 };
 
-TgBot::GenericReply::Ptr keyboard(FSM_STATE state)
+TgBot::GenericReply::Ptr keyboard(FSM state)
 {
     InlineKeyboardMarkup::Ptr keyboard(new InlineKeyboardMarkup);
-
     switch (state)
     {
-    case FSM_STATE::AUTORIZATION_CMPLT: {
-        vector<InlineKeyboardButton::Ptr> row0;
+    case FSM::AUTORIZED: {
         InlineKeyboardButton::Ptr info_help(new InlineKeyboardButton);
         info_help->text = "Информация и помощь";
         info_help->callbackData = "info_help";
-        row0.push_back(info_help);
-        keyboard->inlineKeyboard.push_back(row0);
 
-        vector<InlineKeyboardButton::Ptr> row1;
         InlineKeyboardButton::Ptr rqst_prkng(new InlineKeyboardButton);
         rqst_prkng->text = "Заявка на парковку";
         rqst_prkng->callbackData = "rqst_prkng";
-        row1.push_back(rqst_prkng);
-        keyboard->inlineKeyboard.push_back(row1);
+
+        keyboard->inlineKeyboard.push_back({info_help});
+        keyboard->inlineKeyboard.push_back({rqst_prkng});
     }
     break;
-    case FSM_STATE::RQST_PRKNG: {
-        vector<InlineKeyboardButton::Ptr> row0;
-        InlineKeyboardButton::Ptr submit_rqst(new InlineKeyboardButton);
-        submit_rqst->text = "Подать заявку";
-        submit_rqst->callbackData = "submit_rqst";
-        row0.push_back(submit_rqst);
-        keyboard->inlineKeyboard.push_back(row0);
-
-        vector<InlineKeyboardButton::Ptr> row1;
+    case FSM::RQST_PRKNG: {
         InlineKeyboardButton::Ptr cncl_rqst(new InlineKeyboardButton);
         cncl_rqst->text = "Отменить заявку";
         cncl_rqst->callbackData = "cncl_rqst";
-        row1.push_back(cncl_rqst);
-        keyboard->inlineKeyboard.push_back(row1);
+
+        InlineKeyboardButton::Ptr submit_rqst(new InlineKeyboardButton);
+        submit_rqst->text = "Подать заявку";
+        submit_rqst->callbackData = "submit_rqst";
+
+        keyboard->inlineKeyboard.push_back({cncl_rqst});
+        keyboard->inlineKeyboard.push_back({submit_rqst});
     }
     break;
-    case FSM_STATE::CNCL_RQST: {
-        vector<InlineKeyboardButton::Ptr> row0;
+    case FSM::CNCL_RQST: {
         InlineKeyboardButton::Ptr yes(new InlineKeyboardButton);
         yes->text = "Да";
-        yes->callbackData = "yes";
-        row0.push_back(yes);
-       
+        yes->callbackData = "cncl_rqst";
+
         InlineKeyboardButton::Ptr no(new InlineKeyboardButton);
         no->text = "Нет";
-        no->callbackData = "no";
-        row0.push_back(no);
+        no->callbackData = "submit_rqst";
 
-        keyboard->inlineKeyboard.push_back(row0);
+        keyboard->inlineKeyboard.push_back({yes});
+        keyboard->inlineKeyboard.push_back({no});
     }
     break;
-
     default:
         break;
     }
     return keyboard;
 }
 
+std::string text(FSM state)
+{
+    std::string text;
+    switch (state)
+    {
+    case FSM::START: {
+        text.assign("Введите логин и пароль через пробел:");
+    }
+    break;
+    case FSM::AUTORIZED: {
+        text.assign("Авторизация прошла успешно");
+    }
+    break;
+    case FSM::INFO_HELP: {
+        text.assign("Это бот-помощник для организации очередности парковки!");
+    }
+    break;
+    case FSM::RQST_PRKNG: {
+        text.assign("Выберите, что вы хотите сделать на данный момент:");
+    }
+    break;
+    case FSM::CNCL_RQST: {
+        text.assign("Ваше место Е3, время - с 14:30 до 16:00. Вы действительно хотите отменить бронь?");
+    }
+    break;
+    case FSM::SUBMIT_RQST: {
+        text.assign("На данный момент свободно 3 места, время - с 12:40 до 16:35. Выберите время:");
+    }
+    break;
+
+    default:
+        break;
+    }
+    return std::move(text);
+}
+
 int main(int argc, char **argv)
 {
     string token(argv[1]);
     Bot bot(token);
-    FSM_STATE state{FSM_STATE::START};
+    MenuButton::Ptr menu_start{new MenuButtonCommands};
+    bot.getApi().setChatMenuButton(0, menu_start);
+    FSM state{FSM::START};
 
-    // bot.getEvents().onCommand("start", [&bot, &keyboard](Message::Ptr message) {
-    //     bot.getApi().sendMessage(message->chat->id, "Hi!", false, 0, keyboard);
-    // });
-    // bot.getEvents().onCommand("check", [&bot, &keyboard](Message::Ptr message) {
-    //     string response = "ok";
-    //     bot.getApi().sendMessage(message->chat->id, response, false, 0, keyboard, "Markdown");
-    // });
-    // bot.getEvents().onCallbackQuery([&bot, &keyboard](CallbackQuery::Ptr query) {
-    //     if (StringTools::startsWith(query->data, "check"))
-    //     {
-    //         string response = "ok";
-    //         bot.getApi().sendMessage(query->message->chat->id, response, false, 0, keyboard, "Markdown");
-    //     }
-    // });
-    // ###########################################################
-
+    // ####################################################################################
+    // ####################################################################################
     bot.getEvents().onCommand("start", [&bot, &state](Message::Ptr message) {
-        if (state == FSM_STATE::START)
-        {
-            state = FSM_STATE::AUTORIZATION;
-            bot.getApi().sendMessage(message->chat->id, "Авторизация. \nВведите логин и пароль через пробел: ");
-        }
+        state = FSM::START;
+        ReplyKeyboardRemove::Ptr rm{new ReplyKeyboardRemove};
+        bot.getApi().sendMessage(message->chat->id, text(state), false, 0, keyboard(state));
     });
 
-    bot.getEvents().onAnyMessage([&bot, &state](Message::Ptr message) {
+    bot.getEvents().onCommand("step_back", [&bot, &state](Message::Ptr message) {
         switch (state)
         {
-        case FSM_STATE::AUTORIZATION: {
-            // обработка логина и пароля
-            if (message->text == "login password")
-            {
-                state = FSM_STATE::AUTORIZATION_CMPLT;
-                bot.getApi().sendMessage(message->chat->id, "Авторизация прошла успешно", false, 0, keyboard(state));
-            }
-            else
-                bot.getApi().sendMessage(message->chat->id, "Пароль или логин неверны");
+        case FSM::AUTORIZED: {
+            state = FSM::START;
+            // TODO ! move  bot.getApi().sendMessage just after the end of switch (mb no)
+            bot.getApi().sendMessage(message->chat->id, text(state), false, 0, keyboard(state));
+        }
+        break;
+        case FSM::INFO_HELP: {
+            state = FSM::AUTORIZED;
+            bot.getApi().sendMessage(message->chat->id, text(state), false, 0, keyboard(state));
+        }
+        break;
+        case FSM::RQST_PRKNG: {
+            state = FSM::AUTORIZED;
+            bot.getApi().sendMessage(message->chat->id, text(state), false, 0, keyboard(state));
+        }
+        break;
+        case FSM::CNCL_RQST: {
+            state = FSM::RQST_PRKNG;
+            bot.getApi().sendMessage(message->chat->id, text(state), false, 0, keyboard(state));
+        }
+        break;
+        case FSM::SUBMIT_RQST: {
+            state = FSM::RQST_PRKNG;
+            bot.getApi().sendMessage(message->chat->id, text(state), false, 0, keyboard(state));
         }
         break;
 
@@ -130,28 +155,57 @@ int main(int argc, char **argv)
             break;
         }
     });
+    // ####################################################################################
+    // ####################################################################################
+    bot.getEvents().onAnyMessage([&bot, &state](Message::Ptr message) {
+        switch (state)
+        {
+        case FSM::START: {
+            if (message->text == "log pass")
+            {
+                state = FSM::AUTORIZED;
+                bot.getApi().sendMessage(message->chat->id, text(state), false, 0, keyboard(state));
+            }
+        }
+        break;
 
+        default:
+            break;
+        }
+    });
+    // ####################################################################################
+    // ####################################################################################
     bot.getEvents().onCallbackQuery([&bot, &state](CallbackQuery::Ptr query) {
-        if (StringTools::startsWith(query->data, "info_help") && state == FSM_STATE::AUTORIZATION_CMPLT)
+        switch (state)
         {
-            state = FSM_STATE::INFO_HELP;
-            string response = "Это бот для организации парковки. По вопросам пишите @TEST";
-            bot.getApi().sendMessage(query->message->chat->id, response);
+        case FSM::AUTORIZED: {
+            if (StringTools::startsWith(query->data, "info_help"))
+            {
+                state = FSM::INFO_HELP;
+                bot.getApi().sendMessage(query->message->chat->id, text(state), false, 0, keyboard(state));
+            }
+            if (StringTools::startsWith(query->data, "rqst_prkng"))
+            {
+                state = FSM::RQST_PRKNG;
+                bot.getApi().sendMessage(query->message->chat->id, text(state), false, 0, keyboard(state));
+            }
         }
-        if (StringTools::startsWith(query->data, "rqst_prkng") && state == FSM_STATE::AUTORIZATION_CMPLT)
-        {
-            state = FSM_STATE::RQST_PRKNG;
-            bot.getApi().sendMessage(query->message->chat->id, "?", false, 0, keyboard(state));
+        break;
+        case FSM::RQST_PRKNG: {
+            if (StringTools::startsWith(query->data, "cncl_rqst"))
+            {
+                state = FSM::CNCL_RQST;
+                bot.getApi().sendMessage(query->message->chat->id, text(state), false, 0, keyboard(state));
+            }
+            if (StringTools::startsWith(query->data, "submit_rqst"))
+            {
+                state = FSM::SUBMIT_RQST;
+                bot.getApi().sendMessage(query->message->chat->id, text(state), false, 0, keyboard(state));
+            }
         }
-        if (StringTools::startsWith(query->data, "submit_rqst") && state == FSM_STATE::RQST_PRKNG)
-        {
-            state = FSM_STATE::SUBMIT_RQST;
-            bot.getApi().sendMessage(query->message->chat->id, "Список свободных мест: \nE1\nE2\nE3\nE4");
-        }
-        if (StringTools::startsWith(query->data, "cncl_rqst") && state == FSM_STATE::RQST_PRKNG)
-        {
-            state = FSM_STATE::CNCL_RQST;
-            bot.getApi().sendMessage(query->message->chat->id, "Твое место - E3 c 14:12 по 18:19, отменить заявку?", false, 0, keyboard(state));
+        break;
+        default:
+            break;
         }
     });
 
